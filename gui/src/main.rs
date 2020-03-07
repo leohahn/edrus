c1119ed1de6ffab662725d6662b62a9cd67115d6
Leonardo Hahn <leonnardo.hahn@gmail.com>
2020-03-07 13:30:37 -0300

    drawing stupid cursor

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,3 +1,4 @@
+extern crate cgmath;
 extern crate edrus;
 extern crate env_logger;
 extern crate log;
@@ -9,7 +10,7 @@ extern crate wgpu_glyph;
 extern crate winit;
 extern crate zerocopy;
 
-use na::{Vector2, Vector4};
+// use na::{Matrix3, Matrix4, Point3, Point4, Vector2, Vector3, Vector4};
 use std::mem;
 use wgpu_glyph::{GlyphBrushBuilder, Scale, Section};
 use winit::{
@@ -17,6 +18,14 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, -1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
 
 // #[derive(Debug, Copy, Clone)]
 // #[repr(C)]
@@ -41,22 +50,58 @@ use winit::{
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 struct UBO {
-    color: Vector4<f32>,
+    color: cgmath::Vector4<f32>,
 }
 
-fn create_cursor() -> Vec<Vector2<f32>> {
-    vec![
-        Vector2::new(-1.0, 0.0),
-        Vector2::new(1.0, 0.0),
-        Vector2::new(1.0, 1.0),
-        Vector2::new(1.0, 1.0),
-        Vector2::new(-1.0, 1.0),
-        Vector2::new(-1.0, 0.0),
-    ]
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct CoordinateUniformBuffer {
+    view_projection: cgmath::Matrix4<f32>,
+    model: cgmath::Matrix4<f32>,
+}
+
+fn create_cursor(origin: (f32, f32)) -> Vec<cgmath::Vector2<f32>> {
+    let (xo, yo) = dbg!(origin);
+    let width = 100.0;
+    let height = 100.0;
+    dbg!(vec![
+        cgmath::Vector2::new(xo, yo),
+        cgmath::Vector2::new(xo + width, yo),
+        cgmath::Vector2::new(xo + width, yo + height),
+        cgmath::Vector2::new(xo + width, yo + height),
+        cgmath::Vector2::new(xo, yo + height),
+        cgmath::Vector2::new(xo, yo),
+    ])
+}
+
+fn create_view_projection_matrix(
+    width: u32,
+    height: u32,
+    eye: cgmath::Point3<f32>,
+) -> cgmath::Matrix4<f32> {
+    dbg!(width);
+    dbg!(height);
+    // let target = eye + cgmath::Vector3::new(0.0, 0.0, -1.0);
+    // let eye = cgmath::Point3::new(0.0, 0.0, 1.0);
+    let target = eye - cgmath::Vector3::new(0.0, 0.0, 1.0);
+    // let view = cgmath::Matrix4::look_at_rh(&eye, &target, &cgmath::Vector3::new(0.0, 1.0, 0.0));
+    let view = cgmath::Matrix4::look_at(eye, target, cgmath::Vector3::new(0.0, 1.0, 0.0));
+    let projection = cgmath::ortho(
+        0.0,           // left
+        width as f32,  // right
+        height as f32, // bottom
+        0.0,           // top
+        0.1,           // znear
+        100.0,         // zfar
+    );
+    // opengl_to_wgpu_matrix() * projection * view
+    OPENGL_TO_WGPU_MATRIX * projection * view
 }
 
 fn main() {
-    std::env::set_var("RUST_LOG", "edrus=debug");
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "edrus=debug");
+    }
     env_logger::init();
 
     let filepath = match std::env::args().nth(1) {
@@ -67,8 +112,6 @@ fn main() {
         }
     };
 
-    // let file_contents = std::fs::read_to_string(filepath).unwrap();
-    // let mut table = edrus::text_buffer::SimplePieceTable::new(file_contents);
     let mut buffer = edrus::buffer::Buffer::new(filepath).unwrap();
 
     let event_loop = EventLoop::new();
@@ -109,9 +152,9 @@ fn main() {
 
     layout (location = 0) in vec2 a_pos;
 
-    layout (set = 0, binding = 0) uniform UBO {
-        mat4 u_projection;
-        mat4 u_model_view;
+    layout (set = 0, binding = 1) uniform CoordinateUniformBuffer {
+        mat4 u_view_projection;
+        mat4 u_model;
     };
 
     out gl_PerVertex {
@@ -119,7 +162,7 @@ fn main() {
     };
 
     void main() {
-        gl_Position = vec4(a_pos, 0.0, 1.0);
+        gl_Position = u_view_projection * u_model * vec4(a_pos, 0.0, 1.0);
     }
     "#;
 
@@ -171,18 +214,25 @@ fn main() {
             .expect("failed to read fragment shader"),
     );
 
-    let cursor_vertex_data = create_cursor();
+    let cursor_vertex_data = create_cursor((130.0, 130.0));
     let cursor_vertex_buffer = device
         .create_buffer_mapped(cursor_vertex_data.len(), wgpu::BufferUsage::VERTEX)
         .fill_from_slice(&cursor_vertex_data);
 
     let uniform_bind_group_layout =
         device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            bindings: &[wgpu::BindGroupLayoutBinding {
-                binding: 0,
-                visibility: wgpu::ShaderStage::FRAGMENT,
-                ty: wgpu::BindingType::UniformBuffer { dynamic: false },
-            }],
+            bindings: &[
+                wgpu::BindGroupLayoutBinding {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::FRAGMENT,
+                    ty: wgpu::BindingType::UniformBuffer { dynamic: false },
+                },
+                wgpu::BindGroupLayoutBinding {
+                    binding: 1,
+                    visibility: wgpu::ShaderStage::VERTEX,
+                    ty: wgpu::BindingType::UniformBuffer { dynamic: false },
+                },
+            ],
         });
 
     let uniform_buffer_size = mem::size_of::<UBO>() as wgpu::BufferAddress;
@@ -191,15 +241,31 @@ fn main() {
         usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
     });
 
+    let coordinate_uniform_buffer_size =
+        dbg!(mem::size_of::<CoordinateUniformBuffer>() as wgpu::BufferAddress);
+    let coordinate_uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        size: coordinate_uniform_buffer_size,
+        usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+    });
+
     let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         layout: &uniform_bind_group_layout,
-        bindings: &[wgpu::Binding {
-            binding: 0,
-            resource: wgpu::BindingResource::Buffer {
-                buffer: &uniform_buffer,
-                range: 0..uniform_buffer_size,
+        bindings: &[
+            wgpu::Binding {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &uniform_buffer,
+                    range: 0..uniform_buffer_size,
+                },
             },
-        }],
+            wgpu::Binding {
+                binding: 1,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &coordinate_uniform_buffer,
+                    range: 0..coordinate_uniform_buffer_size,
+                },
+            },
+        ],
     });
 
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
@@ -231,7 +297,7 @@ fn main() {
         depth_stencil_state: None,
         index_format: wgpu::IndexFormat::Uint16,
         vertex_buffers: &[wgpu::VertexBufferDescriptor {
-            stride: mem::size_of::<Vector2<f32>>() as wgpu::BufferAddress,
+            stride: mem::size_of::<cgmath::Vector2<f32>>() as wgpu::BufferAddress,
             step_mode: wgpu::InputStepMode::Vertex,
             attributes: &[wgpu::VertexAttributeDescriptor {
                 offset: 0,
@@ -256,6 +322,10 @@ fn main() {
 
     let mut swap_chain = device.create_swap_chain(&surface, &sc_descriptor);
 
+    let mut eye = cgmath::Point3::new(50.0, 50.0, 1.0);
+    let mut view_projection_matrix =
+        create_view_projection_matrix(sc_descriptor.width, sc_descriptor.height, dbg!(eye));
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
@@ -267,6 +337,8 @@ fn main() {
                 sc_descriptor.width = size.width;
                 sc_descriptor.height = size.height;
                 swap_chain = device.create_swap_chain(&surface, &sc_descriptor);
+                view_projection_matrix =
+                    create_view_projection_matrix(sc_descriptor.width, sc_descriptor.height, eye);
                 window.request_redraw();
             }
             event::Event::RedrawRequested(_) => {
@@ -293,22 +365,43 @@ fn main() {
                         depth_stencil_attachment: None,
                     });
                 }
-                // Draw the cursor
+                // Draw the cursor before everything else, since it should be behind
+                // the text.
                 {
-                    let ubo = UBO {
-                        color: Vector4::new(1.0, 0.0, 0.0, 1.0),
-                    };
-                    let temp_buf = device
-                        .create_buffer_mapped(1, wgpu::BufferUsage::COPY_SRC)
-                        .fill_from_slice(&[ubo]);
+                    {
+                        let ubo = UBO {
+                            color: cgmath::Vector4::new(1.0, 0.0, 0.0, 1.0),
+                        };
+                        let temp_buf = device
+                            .create_buffer_mapped(1, wgpu::BufferUsage::COPY_SRC)
+                            .fill_from_slice(&[ubo]);
 
-                    encoder.copy_buffer_to_buffer(
-                        &temp_buf,
-                        0,
-                        &uniform_buffer,
-                        0,
-                        mem::size_of::<UBO>() as wgpu::BufferAddress,
-                    );
+                        encoder.copy_buffer_to_buffer(
+                            &temp_buf,
+                            0,
+                            &uniform_buffer,
+                            0,
+                            mem::size_of::<UBO>() as wgpu::BufferAddress,
+                        );
+                    }
+                    {
+                        use cgmath::SquareMatrix;
+                        let coordinate_ubo = CoordinateUniformBuffer {
+                            view_projection: view_projection_matrix,
+                            // view_projection: cgmath::Matrix4::identity(),
+                            model: cgmath::Matrix4::identity(),
+                        };
+                        let temp_buf = device
+                            .create_buffer_mapped(1, wgpu::BufferUsage::COPY_SRC)
+                            .fill_from_slice(&[coordinate_ubo]);
+                        encoder.copy_buffer_to_buffer(
+                            &temp_buf,
+                            0,
+                            &coordinate_uniform_buffer,
+                            0,
+                            mem::size_of::<CoordinateUniformBuffer>() as wgpu::BufferAddress,
+                        );
+                    }
 
                     let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                         color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
@@ -336,7 +429,7 @@ fn main() {
                     screen_position: (30.0, 30.0),
                     color: [1.0, 1.0, 1.0, 1.0],
                     scale: Scale { x: 15.0, y: 15.0 },
-                    bounds: (size.width as f32, size.height as f32),
+                    //bounds: (size.width as f32, size.height as f32),
                     ..Section::default()
                 });
 
@@ -350,6 +443,7 @@ fn main() {
                         // size.height.round() as u32,
                         sc_descriptor.width,
                         sc_descriptor.height,
+                        // view_projection_matrix.into::<[[f32; 4]; 4]>(),
                     )
                     .expect("Draw queued");
 
@@ -363,6 +457,24 @@ fn main() {
                     match vk {
                         event::VirtualKeyCode::J => {
                             buffer.move_down();
+                            window.request_redraw();
+                        }
+                        event::VirtualKeyCode::A => {
+                            eye += cgmath::Vector3::new(3.0, 0.0, 0.0);
+                            view_projection_matrix = create_view_projection_matrix(
+                                sc_descriptor.width,
+                                sc_descriptor.height,
+                                eye,
+                            );
+                            window.request_redraw();
+                        }
+                        event::VirtualKeyCode::D => {
+                            eye -= cgmath::Vector3::new(3.0, 0.0, 0.0);
+                            view_projection_matrix = create_view_projection_matrix(
+                                sc_descriptor.width,
+                                sc_descriptor.height,
+                                eye,
+                            );
                             window.request_redraw();
                         }
                         _ => {}
@@ -380,12 +492,12 @@ fn main() {
     });
 }
 
-fn orthographic_projection(width: u32, height: u32) -> [f32; 16] {
-    #[cfg_attr(rustfmt, rustfmt_skip)]
-    [
-        2.0 / width as f32, 0.0, 0.0, 0.0,
-        0.0, 2.0 / height as f32, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        -1.0, -1.0, 0.0, 1.0,
-    ]
-}
+// fn orthographic_projection(width: u32, height: u32) -> [f32; 16] {
+//     #[cfg_attr(rustfmt, rustfmt_skip)]
+//     [
+//         2.0 / width as f32, 0.0, 0.0, 0.0,
+//         0.0, 2.0 / height as f32, 0.0, 0.0,
+//         0.0, 0.0, 1.0, 0.0,
+//         -1.0, -1.0, 0.0, 1.0,
+//     ]
+// }
