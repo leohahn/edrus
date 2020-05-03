7b520ab0e9cccb84e0c65c3201eebff7ca530cff
Leonardo Hahn <leonnardo.hahn@gmail.com>
2020-05-02 21:32:06 -0300

    add keyboard module

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -12,6 +12,7 @@ extern crate serde_derive;
 extern crate winit;
 
 mod footer;
+mod keyboard;
 mod rendering;
 mod scripting;
 
@@ -66,97 +67,6 @@ impl<'a> FontCache<'a> {
 
     fn v_metrics(&self) -> rusttype::VMetrics {
         self.font.v_metrics(self.scale)
-    }
-}
-
-#[derive(Debug, Clone)]
-enum KeyState {
-    InitialDelay { start: Instant, delay: Duration },
-    Repeat { count: u64 },
-    Released,
-}
-
-impl KeyState {
-    fn is_repeat(&self) -> bool {
-        match *self {
-            KeyState::Repeat { .. } => true,
-            _ => false,
-        }
-    }
-
-    fn was_just_pressed(&self) -> bool {
-        match *self {
-            KeyState::InitialDelay { start, .. } if start.elapsed().as_millis() == 0 => true,
-            _ => false,
-        }
-    }
-}
-
-struct Keyboard {
-    keys: HashMap<VirtualKeyCode, KeyState>,
-    initial_delay: Duration,
-}
-
-impl Keyboard {
-    fn new(initial_delay: Duration) -> Self {
-        Keyboard {
-            keys: HashMap::new(),
-            initial_delay: initial_delay,
-        }
-    }
-
-    fn transition<F>(&mut self, key: VirtualKeyCode, mut func: F)
-    where
-        F: FnMut(&KeyState) -> KeyState,
-    {
-        self.keys
-            .entry(key)
-            .and_modify(|state| {
-                let new_state = func(state);
-                *state = new_state;
-            })
-            .or_insert(KeyState::InitialDelay {
-                start: std::time::Instant::now(),
-                delay: self.initial_delay,
-            });
-    }
-
-    fn key_state(&self, virtual_keycode: &VirtualKeyCode) -> &KeyState {
-        self.keys
-            .get(virtual_keycode)
-            .expect("this call should never fail")
-    }
-
-    fn update(&mut self, now: Instant, key: VirtualKeyCode, element_state: event::ElementState) {
-        let initial_delay = self.initial_delay;
-        self.transition(key, |state| match state {
-            KeyState::InitialDelay { start, delay } => {
-                if element_state == event::ElementState::Released {
-                    KeyState::Released
-                } else if now.duration_since(*start) >= *delay {
-                    KeyState::Repeat { count: 1 }
-                } else {
-                    state.clone()
-                }
-            }
-            KeyState::Repeat { count } => {
-                if element_state == event::ElementState::Released {
-                    KeyState::Released
-                } else {
-                    KeyState::Repeat { count: count + 1 }
-                }
-            }
-            KeyState::Released => {
-                if element_state == event::ElementState::Released {
-                    state.clone()
-                } else {
-                    KeyState::InitialDelay {
-                        start: now,
-                        delay: initial_delay,
-                    }
-                }
-            }
-        });
     }
 }
 
@@ -548,7 +458,7 @@ fn main() {
 
     let widget_pipeline = rendering::WidgetPipeline::new(&device, &shader);
 
-    let mut keyboard = Keyboard::new(Duration::from_millis(200));
+    let mut keyboard = keyboard::Keyboard::new(Duration::from_millis(200));
     let font_scale = Scale {
         x: editor_config.font_scale,
         y: editor_config.font_scale,
